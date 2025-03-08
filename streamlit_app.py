47eced23170720caeba2f590a4c8e215455fddbb
vivekswain05 <vivekswain@gmail.com>
2025-03-08 16:34:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from openai import OpenAI
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 # Title of the app
-st.title("Vivek's ChatBot")
+st.title("Vivek's HealthBot")
 
 # Initialize session state for chat history
 if "messages" not in st.session_state:
